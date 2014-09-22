3ab66ad3ae33a17ef7e96ede755b30646440ab1a
Philip Munksgaard <pmunksgaard@gmail.com>
2014-09-22 12:29:57 +0200

    Remove unused constant

--- Asm/Parser.hs
+++ Asm/Parser.hs
@@ -14,14 +14,6 @@ import Data.Maybe
 
 type Parser = GenParser Char [String]
 
-reservedWords = ["ADC", "AND", "ASL", "BCC", "BCS", "BEQ", "BIT", "BMI", "BNE",
-                 "BPL", "BRK", "BVC", "BVS", "CLC", "CLD", "CLI", "CLV", "CMP",
-                 "CPX", "CPY", "DEC", "DEX", "DEY", "EOR", "INC", "INX", "INY",
-                 "JMP", "JSR", "LDA", "LDX", "LDY", "LSR", "NOP", "ORA", "PHA",
-                 "PHP", "PLA", "PLP", "ROL", "ROR", "RTI", "RTS", "SBC", "SEC",
-                 "SED", "SEI", "STA", "STX", "STY", "TAX", "TAY", "TSX", "TXA",
-                 "TXS", "TYA"]
-
 dec ::  Parser Int
 dec = do
   cs <- many1 digit
